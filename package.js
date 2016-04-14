b6442fa99cd9195dc555abea620437268e0c1032
Paulo Mogollon <wanchopeblanco@gmail.com>
2016-04-14 18:13:26 -0500

    Fixed error on export
    * Bumped version to 1.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'indesign:ganalytics',
-    version: '1.0.0',
+    version: '1.0.2',
     // Brief, one-line summary of the package.
     summary: 'Log page views and events to Google Analytics.',
     // URL to the Git repository containing the source code for this package.
@@ -13,12 +13,14 @@ Package.describe({
 Package.onUse(function(api) {
     api.versionsFrom('1.3');
     api.use('ecmascript');
-    api.mainModule('ganalytics.js', 'client');
+    api.addFiles('ganalytics.js', 'client');
+    api.export('GAnalytics', 'client');
 });
 
 Package.onTest(function(api) {
     api.use('ecmascript');
     api.use('tinytest');
     api.use('indesign:ganalytics');
-    api.mainModule('ganalytics-tests.js');
+    api.addFiles('ganalytics.js');
+    api.export('GAnalytics');
 });
