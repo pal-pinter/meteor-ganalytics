0276cc19174a95e38c5ba23501736bb1160883bd
Pintér Pál <pal@palpinter.com>
2017-01-01 10:30:20 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-    name: 'indesign:ganalytics',
-    version: '1.0.2',
+    name: 'palpinter:ganalytics',
+    version: '1.0.0',
     // Brief, one-line summary of the package.
     summary: 'Log page views and events to Google Analytics.',
     // URL to the Git repository containing the source code for this package.
